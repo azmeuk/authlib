fbe9b26a0b0538a43de2f6f82286bb8dda5cdb34
Hsiaoming Yang <me@lepture.com>
2026-01-15 17:12:42 +0900

    fix(jose): add deprecate messages for jose module

--- authlib/_joserfc_helpers.py
+++ authlib/_joserfc_helpers.py
@@ -13,7 +13,7 @@ from authlib.jose import RSAKey
 
 def import_any_key(data: Any):
     if isinstance(data, (OctKey, RSAKey, ECKey, OKPKey)):
-        deprecate("Please use joserfc to import keys.")
+        deprecate("Please use joserfc to import keys.", version="2.0.0")
         return import_key(data.as_dict(is_private=not data.public_only))
 
     if (
@@ -21,15 +21,25 @@ def import_any_key(data: Any):
         and data.strip().startswith("{")
         and data.strip().endswith("}")
     ):
-        deprecate("Please use OctKey, RSAKey, ECKey, OKPKey, and KeySet directly.")
+        deprecate(
+            "Please use OctKey, RSAKey, ECKey, OKPKey, and KeySet directly.",
+            version="2.0.0",
+        )
         data = json_loads(data)
 
     if isinstance(data, (str, bytes)):
-        deprecate("Please use OctKey, RSAKey, ECKey, OKPKey, and KeySet directly.")
+        deprecate(
+            "Please use OctKey, RSAKey, ECKey, OKPKey, and KeySet directly.",
+            version="2.0.0",
+        )
         return import_key(data)
 
     elif isinstance(data, dict):
         if "keys" in data:
+            deprecate(
+                "Please `KeySet.import_key_set` from `joserfc.jwk` to import jwks.",
+                version="2.0.0",
+            )
             return KeySet.import_key_set(data)
         return import_key(data)
     return data
