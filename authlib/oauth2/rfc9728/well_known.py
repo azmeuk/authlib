4c11112e87acd2b76470d6a4ae7b01b950702133
Éloi Rivard <eloi@yaal.coop>
2026-03-17 18:26:00 +0100

    refactor: rename issuer in resource_identifier

--- authlib/oauth2/rfc9728/well_known.py
+++ authlib/oauth2/rfc9728/well_known.py
@@ -3,17 +3,11 @@
 from authlib.common.urls import urlparse
 
 
-def get_well_known_url(issuer, external=False, suffix="oauth-protected-resource"):
-    """Get well-known URI with issuer via `Section 3.1`_.
-
-    .. _`Section 3.1`: https://tools.ietf.org/html/rfc9728#section-3.1
-
-    :param issuer: URL of the issuer
-    :param external: return full external url or not
-    :param suffix: well-known URI suffix for RFC9728
-    :return: URL
-    """
-    parsed = urlparse.urlparse(issuer)
+def get_well_known_url(
+    resource_identifier, external=False, suffix="oauth-protected-resource"
+):
+    """Get well-known URI from a resource identifier via :rfc:`9728#section-3.1`."""
+    parsed = urlparse.urlparse(resource_identifier)
     path = parsed.path
     if path and path != "/":
         url_path = f"/.well-known/{suffix}{path}"
